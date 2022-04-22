b77ad450e09fb7eeacedd02506e9047c160e205d
kakon4366 <kakonbarman4366@gmail.com>
2022-04-23 02:29:45 +0600

    User update method success

--- src/Pages/Dashboard/UpdateUser/UpdateUser.js
+++ src/Pages/Dashboard/UpdateUser/UpdateUser.js
@@ -1,11 +1,64 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { useParams } from "react-router-dom";
+import useUsers from "../../../Hooks/useUsers";
 
 const UpdateUser = () => {
+	// const [users, setUsers] = useUsers();
 	const { id } = useParams();
+	const { register, handleSubmit } = useForm();
+
+	const onSubmit = (data, e) => {
+		const url = `http://localhost:5000/user/${id}`;
+		fetch(url, {
+			method: "PUT",
+			headers: {
+				"content-type": "application/json",
+			},
+			body: JSON.stringify(data),
+		})
+			.then((res) => res.json())
+			.then((result) => {
+				console.log(result);
+				alert("Update success!");
+				e.target.reset();
+			});
+	};
 	return (
-		<div>
-			<h2>This page is updat user: {id}</h2>
+		<div className="container">
+			<h2 className="text-center">Update User</h2>
+			<div className="w-50 mx-auto">
+				<form
+					className="d-flex flex-column"
+					onSubmit={handleSubmit(onSubmit)}
+				>
+					<input
+						className="mb-3 p-2"
+						placeholder="Full Name"
+						{...register("name", { required: true })}
+					/>
+					<input
+						className="mb-3 p-2"
+						placeholder="user"
+						{...register("user", { required: true })}
+					/>
+					<input
+						className="mb-3 p-2"
+						placeholder="E-mail Address"
+						{...register("email", { required: true })}
+					/>
+					<input
+						className="mb-3 p-2"
+						placeholder="Password"
+						{...register("password", { required: true })}
+					/>
+					<input
+						className="mb-3 py-1 btn btn-primary"
+						type="submit"
+						value="Update User"
+					/>
+				</form>
+			</div>
 		</div>
 	);
 };
